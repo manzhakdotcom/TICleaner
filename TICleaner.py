038d242e867b0a10a0bf394540f9b362f0308593
Sergey Manzhak <manzhak.com@gmail.com>
2018-03-15 16:16:42 +0300

    Add class Clear

--- TICleaner.py
+++ TICleaner.py
@@ -1,74 +1,52 @@
-import os
+﻿import os
 from tkinter import *
 from tkinter.filedialog import *
 from tkinter import ttk
 from tkinter import messagebox
 
 
-def get_list_of_folders(path):
-    items = os.listdir(path)
-    folders = []
-    for item in items:
-        if -1 == item.find('.'):
-            folders.append(item)
-    return folders
+class Clear:
+    def __init__(self, path):
+        if '' != path:
+            self.path = path
+            self.clear()
+        else:
+            App.message(u'Папка не выбрана')
 
+    def clear(self):
+        files_path = self.get_folders_and_files()
+        if not files_path:
+            App.message(u'Папка с таблицами не выбрана')
+        else:
+            for file_path in files_path:
+                text = ''
+                with open(file_path, 'r', encoding="cp866") as file:
+                    for line in file:
+                        if ';@U' not in line:
+                            if '@U' in line:
+                                if u'РК' not in line:
+                                    if u'а' not in line:
+                                        line = ';' + line
+                        text = text + line
+                with open(file_path, 'w', encoding="cp866") as file:
+                    file.write(text)
+            App.message(u'Очищено файлов: ' + str(len(files_path)))
 
-def get_list_of_folders_path(path):
-    folders = get_list_of_folders(path)
-    folders_path = []
-    for folder in folders:
-        folder_path = path + os.sep + folder
-        if os.path.isdir(folder_path):
-            folders_path.append(folder_path)
-    return folders_path
-
-
-def get_ti_path_file_from_folders(path):
-    folders_path = get_list_of_folders_path(path)
-    folders = get_list_of_folders(path)
-    files_path = []
-    for folder_path, folder in zip(folders_path, folders):
-        file_path = folder_path + os.sep + 'TI' + folder + '.asm'
-        if os.path.isfile(file_path):
-            files_path.append(folder_path + os.sep + 'TI' + folder + '.asm')
-    return files_path
-
-
-def open_and_change_ti_files(path):
-    if not get_list_of_folders(path):
-        App.message('Папка с таблицами не выбрана')
-    else:
-        files_path = get_ti_path_file_from_folders(path)
-        print(len(files_path))
-        for file_path in files_path:
-            text = ''
-            with open(file_path, 'r', encoding="OEM") as file:
-                for line in file:
-                    if ';@U' not in line:
-                        if '@U' in line:
-                            if 'РК' not in line:
-                                if 'а' not in line:
-                                    line = ';' + line
-                    text = text + line
-            with open(file_path, 'w', encoding="OEM") as file:
-                file.write(text)
-        App.message('Очищено файлов: ' + str(len(files_path)))
-
-
-
-def parsing_files(path):
-    if '' != path:
-        open_and_change_ti_files(path)
-    else:
-        App.message('Папка не выбрана')
+    def get_folders_and_files(self):
+        files_path = []
+        for root, dirs, files in os.walk(self.path):
+            for file in files:
+                if file.endswith(('.asm', '.ASM')):
+                    if file.startswith(('TI', 'ti')):
+                        files_path.append(os.path.join(root, file))
+        return files_path
 
 
 class App:
     def __init__(self, root):
         self.root = root
         self.menu()
-        self.buttons()
+        self.elements()
 
     def menu(self):
         self.root.option_add('*tearOff', False)
@@ -84,14 +62,15 @@ class App:
         menubar.add_cascade(menu=help_, label=u'Справка')
 
         file.add_command(label=u'Выбрать папку...', command=self.get_path_dir)
+        file.add_command(label=u'Настройки',  command=lambda: self.message(u'В разработке'))
         file.add_separator()
         file.add_command(label=u'Выйти', command=self.root.destroy)
 
-        tools.add_command(label=u'Открыть логфайл', command=lambda:self.message('Логфайла нет'))
+        tools.add_command(label=u'Открыть логфайл', command=lambda: self.message(u'В разработке'))
 
-        help_.add_command(label=u'О программе', command=lambda:self.message('Автор: Манжак С.С.\nВерсия: v0.0.1\n'))
+        help_.add_command(label=u'О программе', command=lambda: self.message(u'Автор: Манжак С.С.\nВерсия: v0.0.2\n'))
 
-    def buttons(self):
+    def elements(self):
 
         frame = Frame(self.root)
         frame.pack(pady=10)
@@ -106,14 +85,12 @@ class App:
         button1 = ttk.Button(frame, text=u'Выбрать', command=lambda: self.get_path_dir())
         button1.grid(row=1, column=3)
 
-        button2 = ttk.Button(frame, text=u'Очистить', command=lambda: parsing_files(self.contents.get()))
+        button2 = ttk.Button(frame, text=u'Очистить', command=lambda: Clear(self.contents.get()))
         button2.grid(pady=15, row=2, column=0, columnspan=4)
-
 
     def get_path_dir(self):
         path = askdirectory()
         self.contents.set(path)
-
 
     @staticmethod
     def message(text):
@@ -124,7 +101,7 @@ def main():
     root = Tk()
     root.geometry('300x130+500+200')
     root.title(u'TICleaner')
-    root.iconbitmap(os.getcwd() + os.path.sep + 'img' + os.path.sep + 'cleaner.ico')
+    root.iconbitmap(os.getcwd() + os.path.sep + os.path.sep + 'icon.ico')
     app = App(root)
     root.mainloop()
 
